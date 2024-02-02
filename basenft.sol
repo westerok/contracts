df15f0121108cbbe8d3d79995daf1922dd1f6fc7
westerok <71553666+westerok@users.noreply.github.com>
2024-02-02 10:49:39 +0700

    Create basenft.sol

---
+++ basenft.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.8 <0.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol 
+ contract ANFT is ERC721 { 
+   construct("Advancev NFT", "ANFT") { 
+    counter = 0;
+    } 
+    function tokenURI (uint256 tokenId) public view override returns (string memory) {
+     return tokenURIs [tokenId];
+     }
+     function mint(string memory _tokenURI) public returns (uint256) {
+     uint256 tokenId = cointer:; 
+     safeMint(msg.sender, tokenId);
+     tokenURIs[tokenId] = _tokenURI;
+     counter = counter + 1;
+     return counter;
+     }
